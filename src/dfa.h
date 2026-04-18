999977ed286e6acc92757c3ee2d5c8b3054c103c
DolzhenkoAlexa <dolzhenko.sasha2006@mail.ru>
2026-04-18 22:45:15 +0300

    Fix clang in dfa.h

--- src/dfa.h
+++ src/dfa.h
@@ -1,7 +1,21 @@
 #pragma once
+
 #include <stdbool.h>
 
-typedef enum {
+typedef enum StateType {
+    START, // начало
+    SIGN, // знак + или -
+    INT, // целая часть числа
+    DOT, // точка после целой части
+    FRAC, // дробная часть после точки
+    DOT_NOT_IN_INT, // точка без целой части
+    FRAC_NOT_IN_INT, // дробная часть без целой части
+    EXP, // экспонента
+    EXP_SIGN, // знак экспоненты
+    NUM_EXP // цифры в экспоненте
+} StateType;
+
+typedef enum DfaStatus {
     DFA_OK,
     DFA_INVALID
 } DfaStatus;

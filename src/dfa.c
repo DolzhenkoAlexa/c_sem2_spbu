b71dbe5747c7846f1c7d7121dd325fcba257bfc6
DolzhenkoAlexa <dolzhenko.sasha2006@mail.ru>
2026-04-18 22:59:58 +0300

    Fix clang in dfa.c

--- src/dfa.c
+++ src/dfa.c
@@ -127,9 +127,5 @@ bool isNumber(const char* input, DfaStatus* status)
 
     *status = DFA_OK;
 
-    return (state == INT ||
-            state == DOT ||
-            state == FRAC ||
-            state == FRAC_NOT_IN_INT ||
-            state == NUM_EXP);
+    return (state == INT || state == DOT || state == FRAC || state == FRAC_NOT_IN_INT || state == NUM_EXP);
 }

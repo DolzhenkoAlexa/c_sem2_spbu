78510c59817826678f3d8d82084a26acc371fbb5
DolzhenkoAlexa <dolzhenko.sasha2006@mail.ru>
2026-04-18 22:45:52 +0300

    Fix clang in dfa.c

--- src/dfa.c
+++ src/dfa.c
@@ -1,31 +1,20 @@
 #include "dfa.h"
 #include <stdbool.h>
 
-static int isDigit(char character)
+static bool isDigit(char character)
 {
     return character >= '0' && character <= '9';
 }
 
 bool isNumber(const char* input, DfaStatus* status)
 {
-    typedef enum StateType {
-        START, // начало
-        SIGN, // знак + или -
-        INT, // целая часть числа
-        DOT, // точка после целой части
-        FRAC, // дробная часть после точки
-        DOT_NOT_IN_INT, // точка без целой части
-        FRAC_NOT_IN_INT, // дробная часть без целой части
-        EXP, // экспонента
-        EXP_SIGN, // знак экспоненты
-        NUM_EXP // цифры в экспоненте
-    } StateType;
-
     StateType state = START;
 
-    for (int i = 0; input[i]; i++) {
+    for (int i = 0; input[i] != '\0'; i++) {
         char character = input[i];
+
         switch (state) {
+
         case START:
             if (character == '+' || character == '-') {
                 state = SIGN;
@@ -137,9 +126,10 @@ bool isNumber(const char* input, DfaStatus* status)
     }
 
     *status = DFA_OK;
-    return state == INT ||
-           state == DOT ||
-           state == FRAC ||
-           state == FRAC_NOT_IN_INT ||
-           state == NUM_EXP;
+
+    return (state == INT ||
+            state == DOT ||
+            state == FRAC ||
+            state == FRAC_NOT_IN_INT ||
+            state == NUM_EXP);
 }
